834bb57195626429d4d3d208a72126531ed9926d
JackCharb <jlcharbonneau@wpi.edu>
2018-03-24 20:33:43 -0400

    basic react native app

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome!</Text>
+        <Text>Thank you for volunttering to use our app.</Text>
+        <Text>We hope you enjoy your time at Acadia.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
