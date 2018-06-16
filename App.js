cc585288ea11f0a1593be88031b623fceda5e4f1
JackCharb <jlcharbonneau@wpi.edu>
2018-06-16 18:43:01 -0400

    Send data points to server

--- App.js
+++ App.js
@@ -8,8 +8,24 @@ export default class App extends React.Component {
     this.state = {
       lat: 0.0,
       long: 0.0,
-      watchID: 0,
+      count: 0,
+      userID: 0,
     };
+
+    {/*Temporary example of request to get unique userID
+       https://bhiqp.ky8.io/devreg*/}
+    fetch('https://www.google.com', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'PSK': '8675309'
+      }),
+      method: 'POST'
+    })
+    .then(response => this.setState({userID: 800}))
+    .catch(error => console.error(error));
   }
 
   componentWillUnmount() {
@@ -18,20 +34,40 @@ export default class App extends React.Component {
 
   render() {
     watchID = navigator.geolocation.watchPosition((position) => {
-      this.setState({
-        lat: position.coords.latitude,
-        long: position.coords.longitude,
-        watchID: watchID,
-      });
+
+      {/*Temporary example of logic used to check if coordinates
+       are within boundaries surrounding the park*/}
+      if ((position.coords.longitude < 90) && (position.coords.longitude > -90)
+          && (position.coords.latitude < 90) && (position.coords.latitude > -90)) {
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+          count: this.state.count + 1,
+        });
+        {/*Temporary example of sending data point to
+        the server*/}
+        fetch('https://www.google.com', {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.state),
+          method: 'POST'
+        })
+        .then(response => this.setState({userID: 800}))
+        .catch(error => console.error(error));
+      }
     },
     (error) => alert(JSON.stringify(error)),
     {enableHighAccuracy: false, distanceFilter: 15})
 
+    {/*Print position to the screen*/}
     return (
       <View style={styles.container}>
         <Text>Latitude: {this.state.lat}</Text>
         <Text>Longitude: {this.state.long}</Text>
-        <Text>watchID: {this.state.watchID}</Text>
+        <Text>count: {this.state.count}</Text>
+        <Text>userID: {this.state.userID}</Text>
       </View>
     );
   }
